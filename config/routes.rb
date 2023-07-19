986a7667a028e2a580eeb8a9a3909359b4a05228
Lucas Mazala <lucas_mazala@hotmail.com>
2023-07-18 22:07:20 -0300

    Mostra um article

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'articles#index'
   
-  get '/articles', to: 'articles#index'
+
+  resources :articles # provê todas as urls do crud. Ex get '/articles', to: 'articles#index' e et '/articles/:id', to: 'articles#show' Aula_5
 end
