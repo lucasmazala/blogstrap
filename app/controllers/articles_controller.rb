b0656ed04e38b557e49f5f3c38c28a3b01dd2c7b
Lucas Mazala <lucas_mazala@hotmail.com>
2023-07-25 22:06:40 -0300

    Exclui um article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy] 
   def index
     @articles = Article.all
   end
 
-  def show 
-    @article = Article.find(params[:id])
-  end
+  def show;  end # aula 8
 
   def new #cria o article vazio
     @article = Article.new
@@ -21,12 +20,10 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit  # aula 7
-    @article = Article.find(params[:id])
-  end
+  def edit; end  # aula 7
+    
 
   def update 
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -34,9 +31,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy # aula 8 
+    @article.destroy
+    redirect_to root_path
+  end
+
   private 
 
   def article_params 
     params.require(:article).permit(:title, :body)
   end
+
+  def set_article 
+    @article = Article.find(params[:id])
+  end
+
 end
